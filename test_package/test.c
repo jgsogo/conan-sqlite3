3215e014c3bec40d6d6a4345822c07747b008e85
Javier G. Sogo <jgsogo@gmail.com>
2016-12-07 21:26:34 +0100

    print msg

--- test_package/test.c
+++ test_package/test.c
@@ -1,7 +1,9 @@
+#include <iostream>
 #include <sqlite3.h>
 #include <string.h>
 
 int main(void)
 {
-	return strcmp("3.14.1", sqlite3_version);
+    std::cout << "SQLite3 test" << std::endl;
+    return strcmp("3.14.1", sqlite3_version);
 }
