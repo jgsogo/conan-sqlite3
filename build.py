17d73ee2947186b69459ce8f65743cec46c3a0af
Javier G. Sogo <jgsogo@gmail.com>
2016-12-08 19:56:01 +0100

    add reference (linked to conanfile) so that package is uploaded to conan.io

--- build.py
+++ build.py
@@ -1,12 +1,13 @@
 import os
 from conan.packager import ConanMultiPackager
-
+from conanfile import SQLite3Conan
 
 username = os.getenv("CONAN_USERNAME", "jgsogo")
+reference = os.getenv("CONAN_REFERENCE", "{}/{}".format(SQLite3Conan.name, SQLite3Conan.version))
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username=username)
+    builder = ConanMultiPackager(username=username, reference=reference)
     builder.add_common_builds()
     print("{} builds ahead!".format(len(builder.builds)))
     builder.run()
